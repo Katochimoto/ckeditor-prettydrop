ffe6465687eeeec7e83fecc5c2389cd1893ebb22
Katochimoto <bufpost@yandex.ru>
2015-08-21 13:37:32 +0300

    Initial commit

--- plugin.js
+++ plugin.js
@@ -6,35 +6,46 @@
         modes: { 'wysiwyg': 1 },
 
         init: function(editor) {
-            var dropBookmark = null;
+            var dropBookmarkStart = null;
 
+            // с индексом 1000 выполняется вставка
+            // закладку надо создать прямо перед вставкой
             editor.on('paste', function(event) {
                 var data = event.data;
 
 				if (data.method === 'drop' && data.dataValue) {
+                    // закладка должна быть создана для range, в который будет выполнена вставка
+                    // иначе закладка создается не в том месте
+                    // поэтому если range нет, создаем его и передаем дальше в data
                     if (!data.range) {
                         data.range = this.getSelection().getRanges()[ 0 ];
                     }
 
+                    // collapsed кажется всегда true
+                    // но проверка не лишняя
                     if (data.range.collapsed) {
-                        dropBookmark = data.range.createBookmark(true);
+                        dropBookmarkStart = data.range.createBookmark();
                     }
 				}
 
-            }, editor, null, 990);
+            }, editor, null, 999);
 
             editor.on('afterPaste', function() {
-                if (dropBookmark) {
-                    var bmRange = this.createRange();
-                    bmRange.moveToBookmark(dropBookmark);
-
+                if (dropBookmarkStart) {
                     var range = this.getSelection().getRanges()[ 0 ];
-                    range.optimize();
-                    bmRange.setEnd(range.endContainer, range.endOffset);
-                    bmRange.optimize();
+                    // если не создать закладку из текущего range после вставки
+                    // выделение будет не точным
+                    var dropBookmarkEnd = range.createBookmark();
+
+                    var bmRange = this.createRange();
+                    bmRange.setStartBefore(dropBookmarkStart.startNode);
+                    bmRange.setEndBefore(dropBookmarkEnd.startNode);
                     bmRange.select();
 
-                    dropBookmark = null;
+                    dropBookmarkStart.startNode.remove();
+                    dropBookmarkEnd.startNode.remove();
+
+                    dropBookmarkStart = null;
                 }
 
             }, editor, null, -1);
